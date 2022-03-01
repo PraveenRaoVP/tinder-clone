5841eb8428a41d26b9b651f2f7d1d6efc34246e8
PraveenRaoVP <praveenraovp01@gmail.com>
2022-03-01 07:54:08 +0530

    added person icon

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+function Header() {
+  return (
+    <div class="header">
+        <h1>I am a header</h1>
+        <PersonIcon />
+    </div>
+  )
+}
+
+export default Header
