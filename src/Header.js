aa66500fd799bab9804896041cf696b2a77433a7
PraveenRaoVP <praveenraovp01@gmail.com>
2022-03-01 11:22:15 +0530

    added border radius, adjusted size of matUI icons and added bg in header

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
-import ChatIcon from '@mui/icons-material/Chat';
+import ForumIcon from '@mui/icons-material/Forum'
 function Header() {
   return (
     <div class="header">
         <div className="header-person">
-            <PersonIcon fontSize="large"/>
+            <PersonIcon fontSize="large" style={{fontSize: 45}}/>
         </div>
         <div className="header-tinder-logo">
             <img src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon-3.png"></img>
         </div>
         <div className="header-chat-icon">
-            <ChatIcon fontSize="large" />
+            <ForumIcon fontSize="large" style={{fontSize: 45}}/>
         </div>
     </div>
   )
