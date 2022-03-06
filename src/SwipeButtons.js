0cae733aea2fd5e0318f012b77b367340e1d6385
PraveenRaoVP <praveenraovp01@gmail.com>
2022-03-06 20:05:00 +0530

    didnt add bottom buttons yet becoz of import error

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './SwipeButtons.css';
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material/IconButton';
 
 function SwipeButtons() {
   return (
-    <div className="swipeButtons"></div>
+    <div className="swipeButtons">
+        <IconButton onClick={swiped} className="swipeButtons_replay">
+            <ReplayIcon fontSize='large' />
+        </IconButton>
+        <IconButton onClick={swiped} className="swipeButtons_close">
+            <CloseIcon fontSize='large' />
+        </IconButton>
+        <IconButton onClick={swiped} className="swipeButtons_star">
+            <StarRateIcon fontSize='large' />
+        </IconButton>
+        <IconButton onClick={swiped} className="swipeButtons_favorite">
+            <FavoriteIcon fontSize='large' />
+        </IconButton>
+        <IconButton onClick={swiped} className="swipeButtons_lightning">
+            <FlashOnIcon fontSize='large' />
+        </IconButton>
+    </div>
   )
 }
 
